ef626e061c37cfce41d95458384d49425de838e0
AndreyZin <zinovand@gmail.com>
2024-02-09 00:40:58 +0300

    feat: added httpx cache

--- pyhoro/api/rambler.py
+++ pyhoro/api/rambler.py
@@ -3,17 +3,21 @@ from typing import Optional
 
 import httpx
 
-from pyhoro.enums import ZodiacTitle
-
+from ..enums import ZodiacTitle
 from ..types import Compatibility, Horoscope
 from .base import BaseHoro
+import hishel
 
 
 class RamblerHoro(BaseHoro):
     _base_url = "https://horoscopes.rambler.ru/api/front"
 
     def __init__(self):
-        self._session = httpx.AsyncClient()
+
+        # self._session = httpx.AsyncClient()
+        self._session = hishel.AsyncCacheClient(
+            storage=hishel.AsyncInMemoryStorage(ttl=60 * 60 * 6, capacity=128),
+        )
 
     async def get_horoscope(self, sign: ZodiacTitle, date: Optional[datetime] = None):
         date = date or datetime.today()
@@ -21,6 +25,7 @@ class RamblerHoro(BaseHoro):
         r = await self._session.get(
             f"{self._base_url}/v1/horoscope/{date.strftime('%Y-%m-%d')}/{sign}",
             follow_redirects=True,
+            extensions={"force_cache": True},
         )
         return Horoscope.model_validate(r.json())
 
@@ -28,6 +33,7 @@ class RamblerHoro(BaseHoro):
         r = await self._session.get(
             f"{self._base_url}/v3/horoscope/compatibility/woman-{her_sign}/man-{his_sign}/",
             follow_redirects=True,
+            extensions={"force_cache": True},
         )
         data = r.json()
         topics = []
